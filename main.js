b0b7f592f5980ac390ec9651619fc31bc7c32078
yuya <yuyatennish@gmail.com>
2017-09-28 20:31:40 +0900

    css途中まで, map

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function mapInit() {
+    var centerPosition = new google.maps.LatLng(35.387415, 139.426093);
+    var option = {
+        zoom : 18,
+        center : centerPosition,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    };
+    //地図本体描画
+    var googlemap = new google.maps.Map(document.getElementById("mapField"), option);
 
+
+    //情報ウインドウオプションの指定
+    var infoWindowOption = {
+    position : centerPosition,  //中心座標
+    content : "慶應SFC サウス食堂"  //ウインドウ内に表示する文字列
+    };
+
+    //情報ウインドウ追加
+    var infoWindow = new google.maps.InfoWindow(infoWindowOption);
+    infoWindow.open(googlemap);
+}
+
+    mapInit();
